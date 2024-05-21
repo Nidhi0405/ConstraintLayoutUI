992ff28c39da81855f1607098778b2e161d7b57a
Nidhi <nidhimaniyar003@gmail.com>
2024-05-21 17:26:44 +0530

    Story Bar

--- app/src/main/java/com/example/constraintlayoutui/MainActivity.java
+++ app/src/main/java/com/example/constraintlayoutui/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.constraintlayoutui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView storiesBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init(){
+        storiesBar = findViewById(R.id.storyRecyclerView);
+        List<Story> stories = new ArrayList<>();
+
+        stories.add(new Story(false));
+        stories.add(new Story(false));
+        stories.add(new Story(false));
+        stories.add(new Story(false));
+        stories.add(new Story(true));
+        stories.add(new Story(false));
+        stories.add(new Story(true));
+
+        StoriesAdapter adapter = new StoriesAdapter(stories, this);
+        storiesBar.setAdapter(adapter);
+        storiesBar.setLayoutManager(new LinearLayoutManager(this,RecyclerView.HORIZONTAL, false));
+        //setting the space between story items
+        storiesBar.addItemDecoration(new StoriesDecoration(10));
+
     }
 }
---
+++ app/src/main/java/com/example/constraintlayoutui/StoriesAdapter.java
@@ -0,0 +1,57 @@
+package com.example.constraintlayoutui;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class StoriesAdapter extends RecyclerView.Adapter<StoriesAdapter.StoriesViewHolder>{
+
+    private List<Story> stories;
+    private Context context;
+
+    public StoriesAdapter(List<Story> stories, Context context) {
+        this.stories = stories;
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public StoriesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.story_recycler_item,parent,false);
+        StoriesViewHolder viewHolder = new StoriesViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StoriesViewHolder holder, int position) {
+        //changing the background color if the story is seen
+        if(stories.get(position).isSeen()){
+            holder.storyOutline.setCardBackgroundColor(context.getResources().getColor(R.color.grey));
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return stories.size();
+    }
+
+    //viewHolder
+    public static class StoriesViewHolder extends RecyclerView.ViewHolder{
+        private CardView storyOutline;
+        public StoriesViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            itemView.findViewById(R.id.storyOutline);
+        }
+    }
+}
