3ad852b89e9cd48da217924ae85ad8ef228092c6
Nidhi <nidhimaniyar003@gmail.com>
2024-06-04 19:24:11 +0530

    Commit

--- app/src/main/java/com/example/constraintlayoutui/MainActivity.java
+++ app/src/main/java/com/example/constraintlayoutui/MainActivity.java
@@ -11,34 +11,30 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView storiesBar;
+    private static int ADD_STORY_TYPE=0;
+    private static int ALL_STORY_TYPE=1;
+
+    StoryAdapter adapter;
+    List<StoryModel> modelList = new ArrayList<>();
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        init();
+        recyclerView = findViewById(R.id.story_recyclerView_id);
+
+        modelList.add(new StoryModel(ADD_STORY_TYPE, "1", R.drawable.user_profile));
+        modelList.add(new StoryModel(ALL_STORY_TYPE, "2", "10", "Mariya", R.drawable.user_profile));
+        modelList.add(new StoryModel(ALL_STORY_TYPE, "3", "11", "Jack", R.drawable.user_profile));
+        modelList.add(new StoryModel(ALL_STORY_TYPE, "4", "12", "Alina", R.drawable.user_profile));
+        modelList.add(new StoryModel(ALL_STORY_TYPE, "5", "13", "Peter", R.drawable.user_profile));
+
+        adapter = new StoryAdapter(modelList);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 
-    private void init(){
-        storiesBar = findViewById(R.id.storiesBar);
-        List<Story> stories = new ArrayList<>();
-
-        stories.add(new Story(false));
-        stories.add(new Story(false));
-        stories.add(new Story(true));
-        stories.add(new Story(true));
-        stories.add(new Story(false));
-        stories.add(new Story(true));
-        stories.add(new Story(false));
-        stories.add(new Story(true));
-
-        StoriesAdapter adapter = new StoriesAdapter(stories, this);
-        storiesBar.setAdapter(adapter);
-        storiesBar.setLayoutManager(new LinearLayoutManager(this,RecyclerView.HORIZONTAL, false));
-        //setting the space between story items
-        storiesBar.addItemDecoration(new StoriesDecoration(10));
-
-    }
 }
